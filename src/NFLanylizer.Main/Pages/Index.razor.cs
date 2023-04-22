791e0ec3b2913c968e1432d8d66b15c27ade7bb2
Don <donavan.olsen@gmail.com>
2023-04-22 15:27:35 -0700

    created initial ui

---
+++ src/NFLanylizer.Main/Pages/Index.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NFLanylizer.Main.Pages;
+
+public partial class Index : ComponentBase
+{
+    public bool showSelectSeason = false;
+
+    public void SelectSeason()
+    {
+        showSelectSeason = true;
+    }
+
+    public void CancelSelectSeason()
+    {
+        showSelectSeason = false;
+    }
+}
